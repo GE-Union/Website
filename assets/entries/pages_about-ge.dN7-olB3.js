a82f12d878b74e11778c610ae465726a5283d986
Nathan Dane <nathandane.dev@gmail.com>
2025-06-10 21:08:36 +0200

    Update resource references

---
+++ assets/entries/pages_about-ge.dN7-olB3.js
@@ -0,0 +1,71 @@
+import{j as e,P as d,n,a as s,y as g,b as a,O as w,x as j,c as y,M as k,R as b,S as f,$ as v,d as z,e as r,i as m,f as q}from"../chunks/chunk-Bveo4xMB.js";/* empty css                      */const x="GE Union",N=[{id:"2EiFMEG40PBm9_fkelqpo"},{id:"xOnFYyuZv5VkMeeKDLyiu",maxWidth:991},{id:"Uvcw_OJqyPoLioZzUbP1x",maxWidth:767},{id:"eCbcXJaLEKSKcnbAPzNF0",maxWidth:479}],_="Favicon_gtxnE_QXZldwugdMJXAuG.ico",S=["Rubik-VariableFont_wght_rrlmmnwM8xol83_Rm9VjL.ttf"],P=[],T=o=>e.jsxs("body",{className:"w-element c1lzvaoj",children:[e.jsx(d,{code:`<style>
+  h1{
+    font-family: Tanker;
+    font-weight: 500;
+    font-size: 5em;
+    color: var(--background);
+    line-height: 1;
+    margin: 0;
+  }
+  h2{
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 1;
+    margin: 0;
+  }
+  p{
+    margin: 0;
+  }
+  
+  .icon-background{
+    background-color: var(--back-red-1);
+    z-index: 1;
+    overflow: hidden;
+    position: relative;
+  }
+  .icon-background::before{
+    content: "";
+    position: absolute;
+    width: 200%; height: 200%;
+    top: -50%; left: -50%;
+    background-image: url('/assets/icon-background_RyiWP8CNKOfskpwMp_P8Y.svg?width=1080&quality=80&format=auto');
+    background-size: 200px 115.4734411085px;
+    background-repeat: repeat;
+    animation: scrollBg 30s linear infinite;
+    z-index: -1;
+    opacity: var(--icon-background-opacity, 3%); mix-blend-mode: luminosity;
+    filter: blur(2px)
+  }
+  
+  .home-top::after{
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 250px;
+    background: linear-gradient(180deg, hsl(from var(--back-red-1) h s l / 0.65) 15%, hsl(from var(--back-red-1) h s l / 0) 100%);
+    z-index: -1;
+  }
+
+  @keyframes scrollBg {
+      0% {
+        background-position: 0 0;
+      }
+      100% {
+        background-position: -200px 115.4734411085px;
+      }
+    }
+
+  .home-top-card:hover .home-top-card-title-icon{
+    width: 38px;
+    padding-left: 3px;
+  }
+
+  @media only screen and (max-width: 767px) {
+    h1{
+      font-size: 3.7em;
+    }
+    h2{
+      font-size: 32px;
+    }
+  }
+</style>`,executeScriptOnCanvas:!0,className:"w-html-embed"}),e.jsxs(n,{className:"w-box cck00sw cd3toq c17nm8vt c1xymrvd c8yo8yx ci03eyw cu8ogtt cm1ds2c c1fxgukz c3q79or c1fhonxu cqlg791 cji7xkv c40iywk codsd31 c16er72m c1qt5xo2 c1uz2t7q c4psa79 c1ldi832 icon-background home-top",children:[e.jsxs(n,{className:"w-box c19h37rc ci03eyw c1nj86ny c1mz59p0 c13f4d8e c1122adb cx911xm c4qqqhz c2z8j1e c19dc97m",children:[e.jsx(s,{href:"/",className:"w-link cmvyqw5 cz7iu34 c40iywk codsd31 c16er72m c1qt5xo2 c3gx87z cqyp7hg c1moglug",children:e.jsx(g,{src:"/assets/GE_Logo_-_Big_AQmTkCh-ue9Xfr1xXdV_k.svg",width:492,height:684,className:"w-image c1g1752z c1l3m6tn c1wjaqd0"})}),e.jsxs(n,{className:"w-box ci03eyw cavbteo c17p08f7 cy2vnym cgykfac c8chaf1 crymv6m ch588tu cd1c1x2 cgjpuuk co3wgvm cjdg5q7",children:[e.jsx(s,{href:"/",className:"w-link c4qqqhz cr2ujrk c8l261o c1ozmz5d c1ho4waj c1122adb c10pf28n c1w0lkxn ch9rsc5 c1et34yr c1tj415q",children:e.jsx(a,{className:"w-text",children:"Home"})}),e.jsx(s,{href:"/course-bank",className:"w-link c4qqqhz cr2ujrk c8l261o c1ozmz5d c1ho4waj c1122adb c10pf28n c1w0lkxn ch9rsc5 c1et34yr c1tj415q",children:e.jsx(a,{className:"w-text",children:"Course Bank"})}),e.jsx(s,{href:"/calendar",className:"w-link c4qqqhz cr2ujrk c8l261o c1ozmz5d c1ho4waj c1122adb c10pf28n c1w0lkxn ch9rsc5 c1et34yr c1tj415q",children:e.jsx(a,{className:"w-text",children:"Calendar"})}),e.jsx(s,{href:"/about-geu",className:"w-link c4qqqhz cr2ujrk c8l261o c1ozmz5d c1ho4waj c1122adb c10pf28n c1w0lkxn ch9rsc5 c1et34yr c1tj415q",children:e.jsx(a,{className:"w-text",children:"About"})})]}),e.jsxs(w,{children:[e.jsx(j,{children:e.jsx(y,{className:"w-button cvuh4zx c1v4vkm5 c1bti4b5 c1jbi97f crebcbz cr2ujrk ciidiay c1kf82bd c7jsqgj ctcp1cq c1r8lktc c1t9gjz2 c7jkqej cn1fibt cnwk8w7 c10ybtws cdxgxee c1wmsojl co1yi26 c1vk95sq c12eb7ae",children:e.jsx(d,{code:'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" width="100%" height="100%" style="display: block;"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5px" d="M2.667 8h10.666M2.667 4h10.666M2.667 12h10.666"/></svg>',className:"w-html-embed"})})}),e.jsx(k,{className:"w-dialog-overlay c15392bw c12qysi7 c1jkbqq5 ch8lzlo c13hlxg9 c5mlbae c17brv9o cvuh4zx cu8ogtt c8nmv6p cwqnf9o c1bm3fk1 c183bdo0",children:e.jsxs(b,{className:"w-dialog-content c5mlbae ci03eyw cu8ogtt c7na04l ctnx82u cngdgqi cs20kd cnkevhm cpq2gwm cql21mx ck11ylk c1a71d48 c18s1ywe c1log017 cyuvfar ctbdlr1 c1kek3tg c1s3e5c3 c1ort9jz cuvlntl c18vlwwr c17sz26a cng5oj6",children:[e.jsxs(n,{"data-ws-tag":"nav",className:"w-box c39ez8k",children:[e.jsxs(n,{className:"w-box ci03eyw cu8ogtt c15abkx5 cfp2jcf",children:[e.jsx(f,{className:"w-dialog-description cy7nrqp c1ujy25t cdhy4s8 cc02v1c ca2fle4 c1kvebu6",children:"Find any page you want on the site"}),e.jsx("h2",{className:"w-element",children:"Navigation"})]}),e.jsxs(n,{className:"w-box ci03eyw c1rb689p c1hbxsx0 cy2vnym cgykfac cu8ogtt cfx1lak c12zhvo c8chaf1 crymv6m cjdg5q7",children:[e.jsx(s,{href:"/",className:"w-link c1sy2opm cr2ujrk c8l261o cy7nrqp c1ho4waj c1122adb c10pf28n c1w0lkxn ch9rsc5 cz5lin5",children:e.jsx(a,{className:"w-text",children:"Home"})}),e.jsx(s,{href:"/course-bank",className:"w-link c1sy2opm cr2ujrk c8l261o cy7nrqp c1ho4waj c1122adb c10pf28n c1w0lkxn ch9rsc5 cz5lin5",children:e.jsx(a,{className:"w-text",children:"Course Bank"})}),e.jsx(s,{href:"/calendar",className:"w-link c1sy2opm cr2ujrk c8l261o cy7nrqp c1ho4waj c1122adb c10pf28n c1w0lkxn ch9rsc5 cz5lin5",children:e.jsx(a,{className:"w-text",children:"Calendar"})}),e.jsx(s,{href:"/about-geu",className:"w-link c1sy2opm cr2ujrk c8l261o cy7nrqp c1ho4waj c1122adb c10pf28n c1w0lkxn ch9rsc5 cz5lin5",children:e.jsx(a,{className:"w-text",children:"About"})})]})]}),e.jsx(v,{className:"w-close-button c58kvwj c9jo736 crviqmx c40fhby c1koy2kb cvrpl4x c1uib4vc ci03eyw c1v4vkm5 c1bti4b5 c1qsi9u3 c1s85o6f c7jkqej cy7nrqp cq3zzid cjdtlt6 cxudh90 c12m3onu",children:e.jsx(d,{code:'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" width="100%" height="100%" style="display: block;"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.5 3 3 12.5M3 3l9.5 9.5"/></svg>',className:"w-html-embed"})})]})})]})]}),e.jsxs(n,{className:"w-box c65isya ck11ylk c19haj7v ci03eyw cu8ogtt c1bti4b5 cve325e c14k8w3n cjdez0h c4psa79 chvq5aa chtpppz",children:[e.jsx("h1",{className:"w-element cyoo8jj cnurt1a c1wzvl6u",children:"about GE"}),e.jsx(a,{className:"w-text cg3nt0s c74dsfz czgmbqe cyoo8jj c1wwlxnr cje5w08 c1bck0pu c16hxlzn c1c73s0v c7u4ssh",children:"Everything you could ever wanna know about General Engineering."})]})]})]}),A=({data:o})=>{const{system:c,resources:l,url:i}=o;return e.jsx(z.Provider,{value:{imageLoader:m,assetBaseUrl:r,resources:l,breakpoints:N,onError:console.error},children:e.jsx(T,{system:c},i)})},C=Object.freeze(Object.defineProperty({__proto__:null,default:A},Symbol.toStringTag,{value:"Module"})),E=({data:o})=>{const{pageMeta:c}=o,{origin:l}=new URL(o.url),i={"@context":"https://schema.org","@type":"WebSite",name:x,url:l};let h=c.socialImageUrl;c.socialImageAssetName&&(h=`${l}${m({src:`${r}/${c.socialImageAssetName}`})}`);const p=c.custom.some(t=>t.property==="twitter:card");return e.jsxs(e.Fragment,{children:[o.url&&e.jsx("meta",{property:"og:url",content:o.url}),e.jsx("title",{children:c.title}),e.jsx("meta",{property:"og:title",content:c.title}),c.description&&e.jsxs(e.Fragment,{children:[e.jsx("meta",{name:"description",content:c.description}),e.jsx("meta",{property:"og:description",content:c.description})]}),e.jsx("meta",{property:"og:type",content:"website"}),e.jsx("meta",{property:"og:site_name",content:x}),h&&e.jsx("meta",{property:"og:image",content:c.socialImageUrl}),e.jsx("script",{type:"application/ld+json",dangerouslySetInnerHTML:{__html:JSON.stringify(i,null,2)}}),c.excludePageFromSearch&&e.jsx("meta",{name:"robots",content:"noindex, nofollow"}),c.custom.map(({property:t,content:u})=>e.jsx("meta",{property:t,content:u},t)),(c.socialImageAssetName!==void 0||c.socialImageUrl!==void 0)&&p===!1&&e.jsx("meta",{property:"twitter:card",content:"summary_large_image"}),e.jsx("link",{rel:"icon",href:m({src:`${r}${_}`})}),S.map(t=>e.jsx("link",{rel:"preload",href:`${r}${t}`,as:"font",crossOrigin:"anonymous"},t)),P.map(t=>e.jsx("link",{rel:"preload",href:`${r}${t}`,as:"image"},t))]})},M=Object.freeze(Object.defineProperty({__proto__:null,Head:E},Symbol.toStringTag,{value:"Module"})),B={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"/renderer/+onRenderClient.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:q}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/about-ge/+Page.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:C}},Head:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/about-ge/+Head.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:M}}};export{B as configValuesSerialized};
