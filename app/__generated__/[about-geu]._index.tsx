c13331243d73c23a99cb2a97b50a0473f2b002b4
Nathan <nathandane.dev@gmail.com>
2025-06-23 19:50:08 +0200

    Fix calendar loading

--- app/__generated__/[about-geu]._index.tsx
+++ app/__generated__/[about-geu]._index.tsx
@@ -11,7 +11,7 @@ import { Tooltip as Tooltip, TooltipTrigger as TooltipTrigger, TooltipContent as
 
       export const projectId = "94456f8c-a847-426a-aec8-16de390bd3eb";
 
-      export const lastPublished = "2025-06-23T17:31:48.631Z";
+      export const lastPublished = "2025-06-23T17:47:29.815Z";
 
       export const siteName = "GE Union";
 
--- app/__generated__/[calendar]._index.tsx
+++ app/__generated__/[calendar]._index.tsx
@@ -11,7 +11,7 @@ import { Dialog as Dialog, DialogTrigger as DialogTrigger, DialogOverlay as Dial
 
       export const projectId = "94456f8c-a847-426a-aec8-16de390bd3eb";
 
-      export const lastPublished = "2025-06-23T17:31:48.631Z";
+      export const lastPublished = "2025-06-23T17:47:29.815Z";
 
       export const siteName = "GE Union";
 
